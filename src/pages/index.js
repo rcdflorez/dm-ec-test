b59aeadf2f5418de582f2a78c55d5cef9bc9e245
Ricardo Florez <system@leapcredit.com>
2021-06-12 23:15:21 -0400

    impldement different ab loading method

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,50 @@
-import React from "react"
+import React, { lazy, useState, useEffect } from "react"
 import "bootstrap/dist/css/bootstrap.min.css"
 //import Cookies from "js-cookie"
-import HeroA from "../components/HeroA"
-import HeroB from "../components/HeroB"
+//import HeroA from "../components/HeroA"
+//import HeroB from "../components/HeroB"
 import Footer from "../components/Footer"
 
 import { useQueryParam, NumberParam, StringParam } from "use-query-params"
 
+const importVariant = variantName =>
+  lazy(() =>
+    import(`../components/${variantName}`).catch(() =>
+      import(`../components/HeroB`)
+    )
+  )
+
 export default function Home() {
   const [name] = useQueryParam("name", StringParam)
   const [amount] = useQueryParam("amount", NumberParam)
+  const [variants, setVariants] = useState({})
+  let VariantList = null
 
-  const group =
-    Math.random() < 0.5 ? (
-      <HeroA name={name} amount={amount} />
-    ) : (
-      <HeroB name={name} amount={amount} />
-    )
+  useEffect(() => {
+    // Actualiza el título del documento usando la API del navegador
 
+    console.log(variants.HeroA)
+    VariantList = variants.HeroA
+    console.log(VariantList)
+  }, [])
+
+  const addVariant = variantName => {
+    if (variants[variantName]) return
+
+    VariantList = importVariant(variantName)
+  }
+
+  //console.log(ChartList)
+  Math.random() < 0.5 ? addVariant("HeroA") : addVariant("HeroB")
+  const isSSR = typeof window === "undefined"
   return (
     <>
-      {group}
+      {}
+      {!isSSR && (
+        <React.Suspense fallback="Loading site...">
+          <div className="row">{VariantList ? <VariantList /> : "Ups..."}</div>
+        </React.Suspense>
+      )}
       <Footer />
     </>
   )
