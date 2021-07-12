5170139eeac46fd2a3039848ce7fc6156e69b346
Ricardo Florez <system@leapcredit.com>
2021-07-12 15:23:21 -0400

    add links

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,6 +27,27 @@ export default function Home() {
     //console.log(variants.HeroA)
     //VariantList = variants.HeroA
     //console.log(VariantList)
+
+    let $_REVECHAT_API = ""
+    window.$_REVECHAT_API ||
+      (function (d, w) {
+        var r = ($_REVECHAT_API = function (c) {
+          r._.push(c)
+        })
+        w.__revechat_account = "3884505"
+        w.__revechat_version = 2
+        r._ = []
+        var rc = d.createElement("script")
+        rc.type = "text/javascript"
+        rc.async = true
+        rc.setAttribute("charset", "utf-8")
+        rc.src =
+          ("https:" == document.location.protocol ? "https://" : "http://") +
+          "static.revechat.com/widget/scripts/new-livechat.js?" +
+          new Date().getTime()
+        var s = d.getElementsByTagName("script")[0]
+        s.parentNode.insertBefore(rc, s)
+      })(document, window)
   }, [])
 
   const addVariant = variantName => {
