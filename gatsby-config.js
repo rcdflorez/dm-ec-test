aa5639d0d11a7bf986616da1813359b473e93af4
Ricardo Florez <system@leapcredit.com>
2021-07-15 17:34:22 -0400

    included gtm plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,5 +35,28 @@ module.exports = {
                 crossOrigin: `use-credentials`,
             },
         },
+        {
+            resolve: "gatsby-plugin-google-tagmanager",
+            options: {
+                id: "GTM-W68PHQ4",
+
+                // Include GTM in development.
+                //
+                // Defaults to false meaning GTM will only be loaded in production.
+                includeInDevelopment: true,
+
+                // datalayer to be set before GTM is loaded
+                // should be an object or a function that is executed in the browser
+                //
+                // Defaults to null
+
+                // Name of the event that is triggered
+                // on every Gatsby route change.
+                //
+                // Defaults to gatsby-route-change
+                // Defaults to false
+                enableWebVitalsTracking: true,
+            },
+        },
     ],
 }
