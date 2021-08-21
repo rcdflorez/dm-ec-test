f5fcd1bbbbad8defa96fa2a2b70ffa206267b4b7
Ricardo Florez <ricardo@Ricardos-Mac-mini.local>
2021-08-21 00:40:18 -0400

    update

---
+++ src/components/CountDown.jsx
@@ -0,0 +1,30 @@
+import React , { useEffect}from "react"
+import "./CountDown.Styles.css"
+export default function CountDown() {
+
+    useEffect(() => {
+
+        var countdownNumberEl = document.getElementById('countdown-number');
+var countdown = 10;
+
+countdownNumberEl.textContent = countdown;
+
+setInterval(function() {
+  countdown = --countdown <= 0 ? 10 : countdown;
+   if(countdown <10){
+       countdownNumberEl.textContent = "0"+countdown;
+   } else{
+    countdownNumberEl.textContent = countdown;
+   }
+  
+}, 1000);
+    },[])
+    return (
+        <><div id="countdown">
+        <div id="countdown-number"></div>
+        <svg>
+          <circle r="70" cx="75" cy="75"></circle>
+        </svg>
+      </div></>)
+
+}
