818ea7a1f205fb1fa2ab648ee76dbc8c91c2c597
Ricardo Florez <ricardo@Ricardos-Mac-mini.local>
2021-08-21 00:51:12 -0400

    update

--- src/components/CountDown.jsx
+++ src/components/CountDown.jsx
@@ -1,30 +1,32 @@
-import React , { useEffect}from "react"
+import React, { useEffect } from "react"
 import "./CountDown.Styles.css"
 export default function CountDown() {
+  useEffect(() => {
+    var countdownNumberEl = document.getElementById("countdown-number")
+    var countdown = 10
 
-    useEffect(() => {
+    countdownNumberEl.textContent = countdown
 
-        var countdownNumberEl = document.getElementById('countdown-number');
-var countdown = 10;
-
-countdownNumberEl.textContent = countdown;
-
-setInterval(function() {
-  countdown = --countdown <= 0 ? 10 : countdown;
-   if(countdown <10){
-       countdownNumberEl.textContent = "0"+countdown;
-   } else{
-    countdownNumberEl.textContent = countdown;
-   }
-  
-}, 1000);
-    },[])
-    return (
-        <><div id="countdown">
+    setInterval(function () {
+      countdown = --countdown <= 0 ? 10 : countdown
+      if (countdown < 10) {
+        countdownNumberEl.textContent = "0" + countdown
+      } else {
+        countdownNumberEl.textContent = countdown
+      }
+      if (countdown <= 9) {
+        window.location.href = "http://dev-application.pavecredit.com/"
+      }
+    }, 1000)
+  }, [])
+  return (
+    <>
+      <div id="countdown">
         <div id="countdown-number"></div>
         <svg>
           <circle r="70" cx="75" cy="75"></circle>
         </svg>
-      </div></>)
-
+      </div>
+    </>
+  )
 }
